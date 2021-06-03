07299dfa11960e777b7b8746cceaedc233aae069
Ahmad Alaa <ahmad.alaa.aa.2021@gmail.com>
2021-06-03 10:07:30 -0700

    updated to auth & database

--- auth.js
+++ auth.js
@@ -1,51 +1,94 @@
+//.. signup parametrs
+var signup_email = document.getElementById("up-email");
+var signup_pass = document.getElementById("up-password");
 
 
-function signup() {
-    var email = document.getElementById("up-email");
-    var pass = document.getElementById("up-password");
+//.. sign in parametrs 
+var signin_email = document.getElementById("in-email");
+var signin_pass = document.getElementById("in-password");
 
-    auth.createUserWithEmailAndPassword(email.value, pass.value)
-        .then((userCredential) => {
-            window.location.href="games.html";
-        })
-        .catch((error) => {
-            var errorMessage = error.message;
-            console.log(errorMessage);
-        });
-}
+//.. signup/in buttons
+var signup = document.getElementById("signup-btn");
+var signin = document.getElementById("signin-btn");
 
 
-function signin() {
-    var email = document.getElementById("in-email");
-    var pass = document.getElementById("in-password");
+//.. signup form parametrs
+var Name = document.getElementById('Name');
+var username = document.getElementById('user@name');
+var age = document.getElementById('Age');
+var email = document.getElementById('up-email');
+var pass = document.getElementById('up-password');
+var pass2 = document.getElementById('pass2');
 
-    auth.signInWithEmailAndPassword(email.value, pass.value)
-        .then((userCredential) => {
-            // Signed in
-            window.location.href="games.html";
-        })
-        .catch((error) => {
-            var errorMessage = error.message;
-            console.log(errorMessage);
-        });
+
+
+const rootRef = db.ref('users');
+
+
+signup.addEventListener('click',(e)=>{
+
+    e.preventDefault();
 
     
-}
 
+    auth.createUserWithEmailAndPassword(signup_email.value, signup_pass.value)
+        .then((userCredential) => {
+            window.location.href="games.html";
+        })
+        .catch((error) => {
+            var errorMessage = error.message;
+            console.log(errorMessage);
+        });
 
-function logout() {
+        const autoId = rootRef.push().key
+        rootRef.child(autoId).set({
+            Name :Name.value,
+            Username : username.value,
+            Age : age.value,
+            Email  : email.value,
+            Password : pass.value
+        });
+
+});
+
+signin.addEventListener('click',(e)=>{
+
+    e.preventDefault();
+
+    auth.signInWithEmailAndPassword(signin_email.value, signin_pass.value)
+    .then((userCredential) => {
+        window.location.href="games.html";
+    })
+    .catch((error) => {
+        var errorMessage = error.message;
+        console.log(errorMessage);
+    });
+
+});
+
+function logout(){
     auth.signOut().then(() => {
         window.location.href="index.html";
     }).catch((error) => {
         alert("error");
     });
 }
-
+  /*  
 auth.onAuthStateChanged(function (user) {
     if (user) {
-        alert('useracticated');
+        userinfo.style.display = "block";
     } else {
-        alert('no active user');
+        userinfo.style.display = 'none';
     }
 });
+*/
 
+function validatePass(){
+    if(pass.value===""){
+        alert('please enter password ');
+    }else if(pass2.value===""){
+        alert('please enter verfication password ');
+    }else if(pass.value != pass2.value){
+        alert('password error');
+    }else return true;
+}
